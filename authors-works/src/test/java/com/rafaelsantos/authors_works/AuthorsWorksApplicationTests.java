543c5ed23be33b9b7d76a33ca0657199970b6797
Rafael Santos <rafa.05125@gmail.com>
2024-06-12 14:53:09 -0300

    Project created

---
+++ authors-works/src/test/java/com/rafaelsantos/authors_works/AuthorsWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelsantos.authors_works;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorsWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
