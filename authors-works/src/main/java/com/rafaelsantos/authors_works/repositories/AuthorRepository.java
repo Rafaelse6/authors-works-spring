66d1dfbb08a3ec66f7b2a642964604e4d4d11627
Rafael Santos <rafa.05125@gmail.com>
2024-06-12 22:03:26 -0300

    AuthorRepository, WorkRepository

--- authors-works/src/main/java/com/rafaelsantos/authors_works/repositories/AuthorRepository.java
+++ authors-works/src/main/java/com/rafaelsantos/authors_works/repositories/AuthorRepository.java
@@ -1,2 +1,7 @@
-package com.rafaelsantos.authors_works.repositories;public interface AuthorRepository {
+package com.rafaelsantos.authors_works.repositories;
+
+import com.rafaelsantos.authors_works.entities.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
--- authors-works/src/main/java/com/rafaelsantos/authors_works/repositories/WorkRepository.java
+++ authors-works/src/main/java/com/rafaelsantos/authors_works/repositories/WorkRepository.java
@@ -1,2 +1,18 @@
-package com.rafaelsantos.authors_works.repositories;public class WorkRepository {
+package com.rafaelsantos.authors_works.repositories;
+
+import com.rafaelsantos.authors_works.entities.Work;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface WorkRepository extends JpaRepository<Work, Long> {
+
+    @Query("SELECT obj FROM Work obj " +
+            "WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<Work> searchByName(String name, Pageable pageable);
+
+    @Query("SELECT obj FROM Work obj " +
+            "WHERE UPPER(obj.description) LIKE UPPER(CONCAT('%', :description, '%'))")
+    Page<Work> searchByDescription(String description, Pageable pageable);
 }
