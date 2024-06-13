9405eb0a13bea3655b66a56a6eaac63cf0f158c1
Rafael Santos <rafa.05125@gmail.com>
2024-06-12 22:04:52 -0300

    Initial AuthorController

--- authors-works/src/main/java/com/rafaelsantos/authors_works/controllers/AuthorController.java
+++ authors-works/src/main/java/com/rafaelsantos/authors_works/controllers/AuthorController.java
@@ -1,2 +1,25 @@
-package com.rafaelsantos.authors_works.controllers;public class AuthorController {
+package com.rafaelsantos.authors_works.controllers;
+
+import com.rafaelsantos.authors_works.entities.DTO.AuthorDTO;
+import com.rafaelsantos.authors_works.services.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value ="/authors")
+public class AuthorController {
+
+    @Autowired
+    private AuthorService authorService;
+
+    @GetMapping
+    public ResponseEntity<List<AuthorDTO>> findAll(){
+        List<AuthorDTO> list = authorService.findAll();
+        return ResponseEntity.ok(list);
+    }
 }
